f34c4f870375497762868596362b2b71b2b9760b
jasperdewinther <jaspersophie10@gmail.com>
2023-03-29 17:30:02 +0200

    added ScaleTranslateMap

--- src/transform.rs
+++ src/transform.rs
@@ -15,4 +15,12 @@ pub enum Map {
         inv_scale_sqr: glam::DVec3,
         inv_twice_scale: glam::DVec3,
     },
+    ScaleTranslateMap {
+        translation: glam::DVec3,
+        scale_values: glam::DVec3,
+        voxel_size: glam::DVec3,
+        scale_values_inverse: glam::DVec3,
+        inv_scale_sqr: glam::DVec3,
+        inv_twice_scale: glam::DVec3,
+    },
 }
